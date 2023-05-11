2154f1ad038bf073b01fa0dcbbc10d7d869ffa73
alexandra <alexandra.leschi@hotmail.fr>
2023-05-11 10:51:23 +0200

    create static pages and errors pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+# Controller Pages statiques + pages erreurs
 class PagesController < ApplicationController
-  def index
-  end
+  def index; end
+  def mentions_legales; end
+  def accessibilite; end
+  def donnees_personnelles; end
+  def plan; end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,20 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+  test "should get plan" do
+    get plan_url
+    assert_response :success
+  end
+  test "should get accessibilite" do
+    get accessibilite_url
+    assert_response :success
+  end
+  test "should get mentions legales" do
+    get mentions_legales_path
+    assert_response :success
+  end
+  test "should get donnees personnelles" do
+    get donnees_personnelles_path
+    assert_response :success
+  end
 end
