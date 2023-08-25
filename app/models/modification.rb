c0ad88f7627182421ec2028a8912c77fec530451
alexandra <alexandra.leschi@hotmail.fr>
2023-08-25 16:20:32 +0200

    modification ordre affichage modifications

--- app/models/modification.rb
+++ app/models/modification.rb
@@ -7,6 +7,6 @@ class Modification < ApplicationRecord
 
   def self.modifications_count_by_controller
     select("DATE(created_at) AS modification_date, statut, organisme_id, user_id, COUNT(*) AS modification_count")
-      .group("DATE(created_at), user_id, organisme_id, statut").order("DATE(created_at) ASC")
+      .group("DATE(created_at), user_id, organisme_id, statut").order("DATE(created_at) DESC")
   end
 end
