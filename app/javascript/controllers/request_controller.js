902dc889a3181ca4d93b6f268ba90ed6708f270a
alexandra <alexandra.leschi@hotmail.fr>
2024-03-19 18:44:59 +0100

    Filter optimization in index

--- app/javascript/controllers/request_controller.js
+++ app/javascript/controllers/request_controller.js
@@ -8,7 +8,43 @@ export default class extends Controller {
     connect() {
     }
 
+    checkTag(event){
+        event.preventDefault();
+        let button = event.currentTarget;
+        let checkbox = button.querySelector('input[type="checkbox"]');
+        checkbox.checked = !checkbox.checked;
+        this.formTarget.requestSubmit();
+    }
     addTagSelected(event){
+        event.preventDefault();
+        const selectedValue = event.currentTarget.value;
+        const selectedName = event.currentTarget.options[event.currentTarget.selectedIndex].text;
+        // Ajouter les tags si n'existe pas déjà
+        const tag = `<li data-action="click->request#removeTagSelected" data-value="${selectedValue}"><button class="fr-tag fr-tag--dismiss" aria-label="Retirer">${selectedName}</button></li>`;
+        const nomTag = event.currentTarget.getAttribute("data-tag");
+        const fieldTag = document.getElementById(nomTag);
+        let checkbox = fieldTag.querySelector('input[type="checkbox"][value="' + selectedValue + '"]');
+        if (checkbox.checked == false){
+            checkbox.checked = !checkbox.checked;
+            fieldTag.insertAdjacentHTML("beforeend", tag);
+        }
+        // Réinitialiser le champ select
+        const selectElement = event.currentTarget.closest(".fr-select-group").querySelector(".fr-select");
+        selectElement.selectedIndex = 0;
+        // submit form
+        this.formTarget.requestSubmit();
+    }
+    removeTagSelected(event){
+        event.preventDefault();
+        const selectedValue = event.currentTarget.getAttribute("data-value");
+        let checkbox = event.currentTarget.parentNode.querySelector('input[type="checkbox"][value="' + selectedValue + '"]');
+        if (checkbox.checked){
+            checkbox.checked = !checkbox.checked;
+        }
+        this.formTarget.requestSubmit()
+    }
+
+    addTagSelected2(event){
         event.preventDefault();
         const elementType = event.currentTarget.tagName.toLowerCase();
         const fieldName = event.currentTarget.getAttribute("data-field");
@@ -42,7 +78,7 @@ export default class extends Controller {
         fieldValue.value = JSON.stringify(selectedValues);
     }
 
-    removeTagSelected(event){
+    removeTagSelected2(event){
         event.preventDefault();
         const selectedValue = event.currentTarget.getAttribute("data-value")
         const fieldName = event.currentTarget.getAttribute("data-field");
