8ae0e1a87c03dd0bb4b54c91fe6323ca387fd675
alexandra <alexandra.leschi@hotmail.fr>
2024-03-26 10:48:51 +0100

    Update storage bucket and default arrow for Ransack
    The `STORAGE_BUCKET_NAME` in the .env file has been updated from `budgetlab-bucket` to `opera-bucket`, ensuring that the latter is now in use. Additionally, the default arrow component in the Ransack configuration file now depicts both up and down arrows, enhancing visual indicators during sorting operations.

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -3,7 +3,7 @@ Ransack.configure do |c|
     c.custom_arrows = {
       up_arrow: '<span class="fr-icon--sm fr-icon-arrow-down-fill" aria-hidden="true"></span>',
       down_arrow: '<span class="fr-icon--sm fr-icon-arrow-up-fill" aria-hidden="true"></span>',
-      default_arrow: '<span class="fr-icon--sm fr-icon-arrow-down-fill" aria-hidden="true"></span>'
+      default_arrow: '<span class="fr-icon--sm fr-icon-arrow-up-fill" aria-hidden="true"></span><span class="fr-icon--sm fr-icon-arrow-down-fill" aria-hidden="true"></span>'
     }
   end
   # c.hide_sort_order_indicators = true
