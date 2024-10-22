8c80424ffdac9696ec3cccb15df12a0b0aea0e4c
alexandra <alexandra.leschi@hotmail.fr>
2024-10-22 15:01:01 +0200

    - remove js from pdf layout - update show

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -16,7 +16,12 @@ WickedPdf.configure do |config|
   # config.exe_path = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
-  # config.enable_local_file_access = true
+  WickedPdf.config ||= {}
+  WickedPdf.config.merge!({
+                            layout: "pdf",
+                            enable_local_file_access: true,
+                          })
+
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
